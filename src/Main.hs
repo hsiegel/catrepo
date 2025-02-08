06e53136d10396a32fc13b324e9e623817936777
hsiegel <holgersiegel74@yahoo.de>
2025-02-08 17:21:08 +0100

    ignore certain files

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,35 @@
-import System.Process (readProcess)
-import System.Exit (exitFailure)
 import System.Directory (doesFileExist)
-import System.IO (appendFile)
+import System.Exit (exitFailure)
+import System.Process (readProcess)
+
+-- import System.IO (appendFile)
+
+skip :: [String]
+skip = [".gitignore", "CHANGELOG.md", "LICENSE", "README.md", "Setup.hs", "stack.yaml", "stack.yaml.lock"]
 
 main :: IO ()
 main = do
-    result <- runGitLsFiles
-    case result of
-        Right files -> mapM_ appendToFile files
-        Left err    -> putStrLn ("Fehler: " ++ err) >> exitFailure
+  result <- runGitLsFiles
+  case result of
+    Right files -> mapM_ appendToFile files
+    Left err -> putStrLn ("Fehler: " ++ err) >> exitFailure
 
 runGitLsFiles :: IO (Either String [String])
 runGitLsFiles = do
-    output <- readProcess "git" ["ls-files"] ""
-    return $ Right (lines output)
+  output <- readProcess "git" ["ls-files"] ""
+  return $ Right (lines output)
 
 appendToFile :: FilePath -> IO ()
 appendToFile file = do
-    exists <- doesFileExist file
-    if exists
-        then do
-            content <- readFile file
-            let header = "--- Datei: " ++ file ++ " ---\n"
-            let footer = "\n--- Ende von " ++ file ++ " ---\n\n"
-            appendFile "total.txt" (header ++ content ++ footer)
-        else putStrLn ("Datei nicht gefunden: " ++ file)
+  exists <- doesFileExist file
+  if exists
+    then
+      if file `elem` skip
+        then putStrLn ("Ignoriere: " ++ file)
+        else do
+          content <- readFile file
+          let header = "--- Datei: " ++ file ++ " ---\n"
+          let footer = "\n--- Ende von " ++ file ++ " ---\n\n"
+          putStrLn ("Füge an: " ++ file)
+          appendFile "allfiles.txt" (header ++ content ++ footer)
+    else putStrLn ("Datei nicht gefunden: " ++ file)
