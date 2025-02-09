f13764ec74b8937b7a2911eb3ee4d3800ab3fd03
hsiegel <holgersiegel74@yahoo.de>
2025-02-09 20:07:01 +0100

    replace old file instead of append

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,37 @@
 import System.Directory (doesFileExist)
 import System.Exit (exitFailure)
+import System.FilePath (takeBaseName)
 import System.Process (readProcess)
 
 -- import System.IO (appendFile)
 
 skip :: [String]
-skip = [".gitignore", "CHANGELOG.md", "LICENSE", "README.md", "Setup.hs", "stack.yaml", "stack.yaml.lock"]
+skip = [".gitignore", "ChangeLog.md", "CHANGELOG.md", "LICENSE", "README.md", "Setup.hs", "stack.yaml", "stack.yaml.lock"]
 
 main :: IO ()
 main = do
+  reponame <- getRepoName
+  let filename = reponame <> "-all.txt"
+  putStrLn ("Erstelle Datei: " ++ filename)
   result <- runGitLsFiles
   case result of
-    Right files -> mapM_ appendToFile files
+    Right files -> do
+      writeFile filename ""
+      mapM_ (appendToFile filename) files
     Left err -> putStrLn ("Fehler: " ++ err) >> exitFailure
+
+getRepoName :: IO String
+getRepoName = do
+  output <- readProcess "git" ["rev-parse", "--show-toplevel"] ""
+  return $ takeBaseName (init output)
 
 runGitLsFiles :: IO (Either String [String])
 runGitLsFiles = do
   output <- readProcess "git" ["ls-files"] ""
   return $ Right (lines output)
 
-appendToFile :: FilePath -> IO ()
-appendToFile file = do
+appendToFile :: String -> FilePath -> IO ()
+appendToFile filename file = do
   exists <- doesFileExist file
   if exists
     then
@@ -31,5 +42,5 @@ appendToFile file = do
           let header = "--- Datei: " ++ file ++ " ---\n"
           let footer = "\n--- Ende von " ++ file ++ " ---\n\n"
           putStrLn ("Füge an: " ++ file)
-          appendFile "allfiles.txt" (header ++ content ++ footer)
+          appendFile filename (header ++ content ++ footer)
     else putStrLn ("Datei nicht gefunden: " ++ file)
